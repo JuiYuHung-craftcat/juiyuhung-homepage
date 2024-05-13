1041ef34116118305978ed311cf5970d80ae5703
craftcat <57040983+JuiYuHung-craftcat@users.noreply.github.com>
2024-05-13 08:51:03 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        blue_primary: "#003A4E",
-        blue_darkest_variation: "#041D26",
-        blue_dark_variation: "#385A66",
-        blue_medium_variation: "#5B808C",
-        blue_light_variation: "#B2CFD9",
-        blue_lightest_variation: "#D1E0E5",
+        terminal_green: "#00BB00",
       },
     },
   },
