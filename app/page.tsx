7cce58af1fcd134d2648dab1fa9194c8cf6c6f0f
JuiYuHung-craftcat <juiyuhunghub@gmail.com>
2024-05-14 20:58:44 +0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
       {allPostsData.map(({ id, date, title }) => (
         <div className="p-2 hover:bg-terminal_green hover:text-black" key={id}>
           <a href={`posts/${id}`}>
-            <p className="font-bold text-lg"># {title}</p>
-            @{date}
+            <p className="font-bold text-lg"># {title}</p>@{date}
           </a>
         </div>
       ))}
