928d56ad75ff8b3db8cd9cc27c4e35aa1f3e7b77
craftcat <57040983+JuiYuHung-craftcat@users.noreply.github.com>
2024-05-14 09:21:07 +0800

    Update header.tsx

--- app/(main)/header.tsx
+++ app/(main)/header.tsx
@@ -4,14 +4,14 @@ export const Header = () => {
       <div className="mx-auto max-w-3xl">
         <nav className="flex items-center gap-12 text-base">
           <a href="/" className="group">
-            <h2 className="font-semibold tracking-tighter p-2 font-mplus text-lg">
-              JuiYuHung
+            <h2 className="font-semibold tracking-tighter p-2 font-mplus text-lg hover:uppercase">
+              [JuiYuHung]$
             </h2>
           </a>
           <div className="items-center gap-6 flex">
-            <a href="/">POSTS</a>
-            <a href="/resume">RESUME</a>
-            <a href="https://github.com/JuiYuHung-craftcat">GITHUB</a>
+            <a className="hover:underline decoration-2" href="/">POSTS</a>
+            <a className="hover:underline decoration-2" href="/resume">RESUME</a>
+            <a className="hover:underline decoration-2" href="https://github.com/JuiYuHung-craftcat">GITHUB</a>
           </div>
         </nav>
       </div>
