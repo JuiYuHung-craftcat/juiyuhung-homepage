1a095f152ab20d02bce4c7e57baf513a936ee480
craftcat <57040983+JuiYuHung-craftcat@users.noreply.github.com>
2024-05-14 09:20:48 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import { getSortedPostsData } from "../lib/posts";
 export default function Home() {
   const allPostsData = getSortedPostsData();
   return (
-    <div className="max-w-3xl mx-auto pl-4 pr-4 pt-16">
+    <div className="max-w-3xl mx-auto px-8 pt-16">
       {allPostsData.map(({ id, date, title }) => (
-        <div className="p-2" key={id}>
+        <div className="p-2 hover:bg-terminal_green hover:text-black" key={id}>
           <a href={`posts/${id}`}>
             <p className="font-bold text-lg"># {title}</p>
-            {id}
-            <br />
-            {date}
+            @{date}
           </a>
         </div>
       ))}
