f6e79727bdb08e8084f15fae8f32fb68b99df033
JuiYuHung-craftcat <juiyuhunghub@gmail.com>
2026-03-01 22:24:43 +0800

    Upload mail animation

--- app/(main)/header.tsx
+++ app/(main)/header.tsx
@@ -35,13 +35,13 @@ export const Header = () => {
           </div>
           <a 
             href="mailto:hi@juiyuhung.com"
-            className="bg-terminal_green text-black px-4 py-2 rounded-lg hover:bg-terminal_green/80 transition-all duration-300 md:flex hidden items-center gap-2 font-semibold"
+            className="bg-terminal_green text-black px-4 py-2 rounded-lg hover:bg-terminal_green/80 transition-all duration-300 flex items-center gap-2 font-semibold"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
               <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
             </svg>
-            hi@juiyuhung.com
+            <span className="md:inline hidden">hi@juiyuhung.com</span>
           </a>
         </nav>
       </div>
